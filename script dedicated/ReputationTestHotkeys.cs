b164cd37f8ccfa9b8159bf939afd10e72f486840
Tevvos <thierrynn12@gmail.com>
2026-03-28 06:35:42 +0400

    Delete script dedicated directory

--- script dedicated/ReputationTestHotkeys.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ReputationTestHotkeys : MonoBehaviour {
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.F6)) ReputationManager.Instance?.AddReputation(10);   // +10
-        if (Input.GetKeyDown(KeyCode.F7)) ReputationManager.Instance?.AddReputation(-10);  // -10 (min 0)
-        if (Input.GetKeyDown(KeyCode.F8)) ReputationManager.Instance?.SetReputation(0);    // reset
-    }
-}
